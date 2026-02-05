0495ccbc40664e6e0caecdaf1d74f42c1cd261fe
Younes Manton <ymanton@ca.ibm.com>
2026-02-05 15:37:44 -0500

    Move tpch queries into a dedicated dir

--- src/main/scala/TpchQuery.scala
+++ src/main/scala/TpchQuery.scala
@@ -73,7 +73,7 @@ object TpchQuery {
     val inputDataDir = sys.env.getOrElse("TPCH_INPUT_DATA_DIR", "file://" + cwd + "/dbgen")
     val queryOutputDir = sys.env.getOrElse("TPCH_QUERY_OUTPUT_DIR", inputDataDir + "/output")
     val executionTimesPath = sys.env.getOrElse("TPCH_EXECUTION_TIMES", cwd + "/tpch_execution_times.txt")
-    val sqlDir = sys.env.getOrElse("TPCH_QUERY_SQL_DIR", cwd + "/src/sql") + "/"
+    val sqlDir = sys.env.getOrElse("TPCH_QUERY_SQL_DIR", cwd + "/src/sql/tpch") + "/"
 
     val spark = SparkSession
       .builder
